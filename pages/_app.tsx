a4a1aa8d6e60a6479684f4fb31da563bd0bcd731
max <mtamb@seas.upenn.edu>
2022-09-03 20:04:34 -0400

    added header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,38 @@ function MyApp({ Component, pageProps }: any) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <header>
+
+  <nav className="navbar navbar-expand-lg navbar-light bg-white">
+    <div className="container-fluid">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarExample01"
+        aria-controls="navbarExample01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i className="fas fa-bars"></i>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarExample01">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item active">
+            <a className="nav-link" aria-current="page" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/form">DropItem</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/pickup">Pickup</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
+</header>
       <ConvexProvider client={convex}>
         <Component {...pageProps} />
       </ConvexProvider>
--- pages/form.tsx
+++ pages/form.tsx
@@ -4,22 +4,27 @@ const TestFunction = () => {
     const submitFreeItem = useMutation("submitFreeItem")
 
     const freeItems = useQuery("getFreeItems") || [];
+    const header = DisplayHeader();
 
     const miniFunction = () => {
         // console.log("Hey hey")
         submitFreeItem("test", 5, 6)
     }
-    return <div><h1>hello</h1>
-        <button onClick={miniFunction}>Our test button</button>
-        {freeItems.map(f => <div className="bs-button" key={f._id.toString()} style={{backgroundColor: 'lightblue'}}>{f.nameOfItem}</div>)}
 
-        </div>
+    return <div> {DisplayHeader()}
+    <div><h1>hello</h1>
+    <a className="btn btn-primary" onClick={miniFunction} role="button" style={{backgroundColor: 'green'}}>Submit Form</a>
+    {freeItems.map(f => <div className="bs-button" key={f._id.toString()} style={{backgroundColor: 'lightgreen'}}>{f.nameOfItem}</div>)}
+    </div></div>
 }
 
-
-
-
-
-
+const DisplayHeader = () => {
+    return <header>
+    <div className="p-5 text-center bg-light">
+        <h1 className="mb-3">DROP</h1>
+        <h4 className="mb-3">Fill out the form below with the info about the item you want to dispose of!</h4>
+    </div>
+    </header>
+}
 
 export default TestFunction
