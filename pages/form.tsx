e5a0a5c9f273f45d089f7e1297305b3e6c8984d8
ruxicl <s2011447@ed.ac.uk>
2022-09-03 22:19:47 -0400

    add form

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,4 +1,6 @@
 import { useMutation, useQuery } from "../convex/_generated/react"
+import React from 'react';
+import { useFormik } from 'formik';
 
 const TestFunction = () => {
     const submitFreeItem = useMutation("submitFreeItem")
@@ -12,6 +14,7 @@ const TestFunction = () => {
     }
 
     return <div> {DisplayHeader()}
+    {SignupForm()}
     <a className="btn btn-primary" onClick={miniFunction} role="button" style={{backgroundColor: 'green'}}>Submit Form</a>
     </div>
 }
@@ -26,3 +29,52 @@ const DisplayHeader = () => {
 }
 
 export default TestFunction
+
+const SignupForm = () => {
+  // Note that we have to initialize ALL of fields with values. These
+  // could come from props, but since we don’t want to prefill this form,
+  // we just use an empty string. If we don’t do this, React will yell
+  // at us.
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
