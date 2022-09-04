aaf752bf66af0cbd4fedf98a76f4928895f7e337
ruxicl <s2011447@ed.ac.uk>
2022-09-03 22:55:22 -0400

    .

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,69 +1,83 @@
 import { useMutation, useQuery } from "../convex/_generated/react"
 import React from 'react';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const TestFunction = () => {
-    const submitFreeItem = useMutation("submitFreeItem")
+  const submitFreeItem = useMutation("submitFreeItem")
 
-    const freeItems = useQuery("getFreeItems") || [];
-    const header = DisplayHeader();
+  const freeItems = useQuery("getFreeItems") || [];
+  const header = DisplayHeader();
 
-    const miniFunction = () => {
-        // console.log("Hey hey")
-        submitFreeItem("test", 5, 6)
-    }
+  const miniFunction = () => {
+    // console.log("Hey hey")
+    submitFreeItem("test", 5, 6)
+  }
 
-    return <div> {DisplayHeader()}
+  return <div> {DisplayHeader()}
     {SignupForm()}
-    <a className="btn btn-primary" onClick={miniFunction} role="button" style={{backgroundColor: 'green'}}>Submit Form</a>
-    </div>
+    <a className="btn btn-primary" onClick={miniFunction} role="button" style={{ backgroundColor: 'green' }}>Submit Form</a>
+  </div>
 }
 
 const DisplayHeader = () => {
-    return <header>
+  return <header>
     <div className="pt-30 pb-30 bg-light lh-40 text-center navigation_8">
-        <h1 className="mb-3">DROP</h1>
-        <h4 className="mb-3">Fill out the form below with the info about the item you want to dispose of!</h4>
+      <h1 className="mb-3">DROP</h1>
+      <h4 className="mb-3">Fill out the form below with the info about the item you want to dispose of!</h4>
     </div>
-    </header>
+  </header>
 }
 
 export default TestFunction
 
 const SignupForm = () => {
-  // Note that we have to initialize ALL of fields with values. These
-  // could come from props, but since we don’t want to prefill this form,
-  // we just use an empty string. If we don’t do this, React will yell
-  // at us.
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
+      boxDescription: '',
+      location: '',
       email: '',
     },
+    validationSchema: Yup.object({
+      boxDescription: Yup.string()
+        .min(5, 'Must be 5 characters or more')
+        .required('Required'),
+      location: Yup.string()
+        .max(20, 'Must be 20 characters or less')
+        .required('Required'),
+      email: Yup.string().email('Invalid email address'),
+    }),
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
     },
   });
   return (
     <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="firstName">First Name</label>
+      <label htmlFor="boxDescription">Box description</label>
       <input
-        id="firstName"
-        name="firstName"
+        id="boxDescription"
+        name="boxDescription"
         type="text"
         onChange={formik.handleChange}
-        value={formik.values.firstName}
+        onBlur={formik.handleBlur}
+        value={formik.values.boxDescription}
       />
+      {formik.touched.boxDescription && formik.errors.boxDescription ? (
+        <div>{formik.errors.boxDescription}</div>
+      ) : null}
 
-      <label htmlFor="lastName">Last Name</label>
+      <label htmlFor="location">Location</label>
       <input
-        id="lastName"
-        name="lastName"
+        id="location"
+        name="location"
         type="text"
         onChange={formik.handleChange}
-        value={formik.values.lastName}
+        onBlur={formik.handleBlur}
+        value={formik.values.location}
       />
+      {formik.touched.location && formik.errors.location ? (
+        <div>{formik.errors.location}</div>
+      ) : null}
 
       <label htmlFor="email">Email Address</label>
       <input
@@ -71,8 +85,12 @@ const SignupForm = () => {
         name="email"
         type="email"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.email}
       />
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
 
       <button type="submit">Submit</button>
     </form>
